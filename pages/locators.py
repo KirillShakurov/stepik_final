a8f7d4d1bbbe43a4900747ef7f5574c7f94dbae5
Kirill Shakurov <shakurov_k@tochka.com>
2026-02-03 12:05:18 +0500

    move login methods to BasePage with inheritance

--- pages/locators.py
+++ pages/locators.py
@@ -2,8 +2,22 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
-        LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
-        LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-        REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
+    PRODUCT_NAME_IN_SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success strong")
+    BASKET_INFO = (By.CSS_SELECTOR, ".alert-info")
+    BASKET_TOTAL_PRICE = (By.CSS_SELECTOR, ".alert-info p strong")
+    PRODUCT_TITLE = (By.CSS_SELECTOR, "h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    
