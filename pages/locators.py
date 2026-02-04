8d1f508acbcb7d91273d0542f9c08562c05a8046
Kirill Shakurov <shakurov_k@tochka.com>
2026-02-04 10:49:27 +0500

    add TestUserAddToBasketFromProductPage with registration setup

--- pages/locators.py
+++ pages/locators.py
@@ -7,6 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTRATION_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_SUBMIT_BUTTON = (By.CSS_SELECTOR, "#register_form button[name='registration_submit']")
 
 class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
@@ -21,8 +25,10 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     VIEW_BASKET_LINK = (By.CSS_SELECTOR, "span.btn-group a.btn-default")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators():
     BASKET_IS_EMPTY_TEXT = (By.CSS_SELECTOR, "#content_inner p")
     PRODUCT_IN_BASKET = (By.CSS_SELECTOR, ".basket-items")
+
     
--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,5 @@
 import pytest
-
+import time
 from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
@@ -50,26 +50,50 @@ from .pages.basket_page import BasketPage
 #     page.add_product_to_basket()
 #     page.should_be_success_message_disappeared()
 
-link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+# link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+#
+# def test_guest_should_see_login_link_on_product_page(browser):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_be_login_link()
+#
+#
+# def test_guest_can_go_to_login_page_from_product_page(browser):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.go_to_login_page()
+#     login_page = LoginPage(browser, browser.current_url)
+#     login_page.should_be_login_page()
+#
+# def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.go_to_basket_page()
+#     basket_page = BasketPage(browser, browser.current_url)
+#     basket_page.should_be_basket_empty()
+#     basket_page.should_not_be_product_in_basket()
 
-def test_guest_should_see_login_link_on_product_page(browser):
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+login_link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(autouse=True)
+    def setup_method(self, browser):
+        self.browser = browser
+        email = str(time.time()) + "@fakemail.org"
+        password = "testpass123"
 
+        self.login_page = LoginPage(browser, login_link)
+        self.login_page.open()
+        self.login_page.register_new_user(email, password)
 
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+        self.product_page = ProductPage(browser, link)
+        self.product_page.open()
+        self.product_page.should_be_authorized_user()
 
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_basket_page()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_be_basket_empty()
-    basket_page.should_not_be_product_in_basket()
+    def test_user_cant_see_success_message(self):
+        self.product_page.should_not_be_success_message()
 
+    def test_user_can_add_product_to_basket(self):
+        self.product_page.add_product_to_basket()
+        self.product_page.should_be_correct_product_name_in_basket_message()
+        self.product_page.should_be_correct_basket_total_price()
